c1ce2595a7b2b0ff3ce454a16b29e6f71d24da1f
Chris Mullany <me@chrismullany.com>
2016-01-07 12:32:07 +0000

    encapsulated room mesh and light into room class, added motion

---
+++ bindu/app/Room.js
@@ -0,0 +1,92 @@
+// A Room has:
+//  - a mesh generated using constructive solid geometry
+//  - a point light
+// it animates towards the camera
+
+var Room = window.Room = function(properties) {
+
+    this.properties = properties || this.propertiesDefault;
+
+    console.log("New room at z" + properties.z);
+
+    // CSG Test
+    //shading: THREE.FlatShading
+    // var roomMaterial = new THREE.MeshNormalMaterial();
+    var roomMaterial = new THREE.MeshPhongMaterial({color:0xffffff, overdraw: 0.5});
+    
+    var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
+    var cubeMesh = new THREE.Mesh( cubeGeometry );
+    var cubeBSP = new ThreeBSP( cubeMesh );
+
+    var barGeometry = new THREE.CubeGeometry( 20, 120, 10, 1, 1, 1 );
+    var barMesh = new THREE.Mesh( barGeometry );
+    var barBSP = new ThreeBSP( barMesh );
+        
+    var sphereGeometry = new THREE.SphereGeometry( 60, 32, 32 );
+    var sphereMesh = new THREE.Mesh( sphereGeometry );
+    var sphereBSP = new ThreeBSP( sphereMesh );
+
+    //
+    var cylinderGeometry = new THREE.CylinderGeometry( 16, 16, 120, 32 );
+    var cylinderMesh = new THREE.Mesh( cylinderGeometry );
+    var cylinderBSP = new ThreeBSP( cylinderMesh );
+    
+    // Example #1 - Cube subtract Sphere
+    // var newBSP = cubeBSP.subtract( cylinderBSP );
+    // var newBSP = sphereBSP.subtract( cylinderBSP );
+    var newBSP = sphereBSP.subtract( barBSP );
+
+    this.roomMesh = newBSP.toMesh( roomMaterial );
+    // this.roomMesh.position = this.properties.position;
+    this.roomMesh.position.set( 0, 0, this.properties.z );
+    this.roomMesh.rotation.x = Math.PI/2;
+    this.roomMesh.material.side = THREE.BackSide;
+    //scene.add( roomMesh );
+
+    // Example #2 - Sphere subtract Cube
+    // var newBSP = sphereBSP.subtract( cubeBSP );
+    // var roomMesh = newBSP.toMesh( roomMaterial );
+    // roomMesh.position.set(180, 60, 0);
+    // scene.add( roomMesh );
+    
+    // Example #3 - Cube union Sphere
+    // var newBSP = sphereBSP.union( cubeBSP );
+    // var roomMesh = newBSP.toMesh( roomMaterial );
+    // roomMesh.position.set(70, 60, -120);
+    // scene.add( roomMesh );
+
+    // Example #4 - Cube intersect Sphere
+    // var newBSP = sphereBSP.intersect( cubeBSP );
+    // var roomMesh = newBSP.toMesh( roomMaterial );
+    // roomMesh.position.set(-70, 60, -120);
+    // scene.add( roomMesh );
+
+
+    // LIGHT
+    var colours = [ 0xF55DB3, 0x7B23AD, 0x3A50C9, 0xF7E51B, 0xFA980F ];
+    var colour = colours[Math.floor(Math.random() * colours.length)]
+    this.light = new THREE.PointLight(colour, 2, 200);
+    // this.light.position = this.properties.position;
+    this.light.position.set( 0, 0, this.properties.z );
+    //scene.add(light);
+
+};
+
+Room.prototype.update = function() {
+    var speed = 0.3;
+    this.roomMesh.translateY(speed);
+    this.light.translateZ(speed);
+};
+
+Room.prototype.getZ = function() {
+    return this.light.position.z;
+};
+
+Room.prototype.reset = function(z) {
+    this.roomMesh.position.set( 0, 0, z );
+    this.light.position.set( 0, 0, z );
+};
+
+Room.prototype.propertiesDefault = {
+    position : new THREE.Vector3( 0,0,0 )
+}
--- bindu/app/main.js
+++ bindu/app/main.js
@@ -9,8 +9,10 @@ var isCardboard;
 var isMouseDown = false;
 var geometry;
 
-var lights = [];
-var lightProps = {intensity : 2.0, distance : 87.0, decay : 0.01};
+var rooms = [];
+var lightProps = {intensity : 1.5, distance : 200.0, decay : 1.0};
+
+// TODO: wrap CSG and light, spawn and animate, kill when at certain z
 
 ///////////////////////////////////////////////////////////////////////////////
 // particles
@@ -19,79 +21,28 @@ function setup() {
     scene = new THREE.Scene();
     //scene.fog = new THREE.FogExp2(0x000000, 0.01);
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 500;
+    camera.position.z = 100;
     renderer = new THREE.WebGLRenderer({
         antialias: false
     });
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setClearColor(0xffffff);
+    renderer.setClearColor(0x3A50C9);
     renderer.setSize(window.innerWidth, window.innerHeight);
     element = renderer.domElement;
     container = document.getElementById('holder');
     container.appendChild(renderer.domElement);
 
-    // CSG Test
-    //shading: THREE.FlatShading
-    // var materialNormal = new THREE.MeshNormalMaterial();
-    var materialNormal = new THREE.MeshPhongMaterial({color:0xffffff, overdraw: 0.5});
-    
-    var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
-    var cubeMesh = new THREE.Mesh( cubeGeometry );
-    var cubeBSP = new ThreeBSP( cubeMesh );
-        
-    var sphereGeometry = new THREE.SphereGeometry( 60, 32, 32 );
-    var sphereMesh = new THREE.Mesh( sphereGeometry );
-    var sphereBSP = new ThreeBSP( sphereMesh );
-
-    var cylinderGeometry = new THREE.CylinderGeometry( 32, 32, 120, 32 );
-    var cylinderMesh = new THREE.Mesh( cylinderGeometry );
-    var cylinderBSP = new ThreeBSP( cylinderMesh );
-    
-    // Example #1 - Cube subtract Sphere
-    // var newBSP = cubeBSP.subtract( sphereBSP );
-    var newBSP = cubeBSP.subtract( cylinderBSP );
-    var newMesh = newBSP.toMesh( materialNormal );
-    newMesh.position.set(0, 0, 0);
-    newMesh.rotation.x = Math.PI/2;
-    newMesh.material.side = THREE.BackSide;
-    scene.add( newMesh );
-
-    // Example #1 - Cube subtract Sphere
-    var newBSP = cubeBSP.subtract( cylinderBSP );
-    var newMesh = newBSP.toMesh( materialNormal );
-    newMesh.position.set(0, 0, -100);
-    newMesh.rotation.x = Math.PI/2;
-    scene.add( newMesh );
-
-    // Example #2 - Sphere subtract Cube
-    // var newBSP = sphereBSP.subtract( cubeBSP );
-    // var newMesh = newBSP.toMesh( materialNormal );
-    // newMesh.position.set(180, 60, 0);
-    // scene.add( newMesh );
-    
-    // Example #3 - Cube union Sphere
-    // var newBSP = sphereBSP.union( cubeBSP );
-    // var newMesh = newBSP.toMesh( materialNormal );
-    // newMesh.position.set(70, 60, -120);
-    // scene.add( newMesh );
-
-    // Example #4 - Cube intersect Sphere
-    // var newBSP = sphereBSP.intersect( cubeBSP );
-    // var newMesh = newBSP.toMesh( materialNormal );
-    // newMesh.position.set(-70, 60, -120);
-    // scene.add( newMesh );
-
-
-    // LIGHT
-    var light = new THREE.PointLight(0xF55DB3, 2, 200);
-    light.position.set(0,0,0);
-    scene.add(light);
-    lights.push(light);
-
-    var light = new THREE.PointLight(0xC25DF5, 2, 200);
-    light.position.set(0,0,-100);
-    scene.add(light);
-    lights.push(light);
+    // setup some rooms here
+    for (var i = 0; i < 5; i++) {
+        var props = {
+            position: new THREE.Vector3(0, 0, i*100),
+            z: -i*100
+        }
+        var room = new Room(props);
+        scene.add( room.roomMesh );
+        scene.add( room.light );
+        rooms.push(room);
+    };
 
     setupGui();
     render();
@@ -122,10 +73,15 @@ function update() {
     // light.distance = lightProps.distance;
     // light.decay = lightProps.decay;
 
-    for (var i = 0; i < lights.length; i++) {
-        lights[i].intensity = lightProps.intensity;
-        lights[i].distance = lightProps.distance;
-        lights[i].decay = lightProps.decay;
+    for (var i = 0; i < rooms.length; i++) {
+        var room = rooms[i];
+        room.update();
+        room.light.intensity = lightProps.intensity;
+        room.light.distance = lightProps.distance;
+        room.light.decay = lightProps.decay;
+        if (room.getZ() > 200) {
+            room.reset(-300)
+        }
     };
 }
 
