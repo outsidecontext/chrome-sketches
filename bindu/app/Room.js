7bc7448c527af59c4a4c80b80250bc5f4d90fdd0
Chris Mullany <me@chrismullany.com>
2016-01-08 18:19:50 +0000

    tidy up of room

--- bindu/app/Room.js
+++ bindu/app/Room.js
@@ -8,28 +8,25 @@ var Room = window.Room = function(properties) {
     this.properties = properties || this.propertiesDefault;
     var colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
     var roomMaterial = new THREE.MeshPhongMaterial({color:colour, fog: true, shininess: 80});
-
     
-    var cubeGeometry = new THREE.BoxGeometry( 100, this.properties.depth, 100, 1, 1, 1 );
-    var cubeMesh = new THREE.Mesh( cubeGeometry );
-    var cubeBSP = new ThreeBSP( cubeMesh );
+    // var cubeGeometry = new THREE.BoxGeometry( 100, this.properties.depth, 100, 1, 1, 1 );
+    // var cubeMesh = new THREE.Mesh( cubeGeometry );
+    // var cubeBSP = new ThreeBSP( cubeMesh );
 
-    var w = randomInRange(10, 20);
-    var h = randomInRange(10, 20);
+    var w, h;
     if (Math.random() > 0.5) {
         w = 15; h = 8;
     }
     else {
         w = 8; h = 15;
     }
-    var barGeometry = new THREE.BoxGeometry( w, this.properties.depth, h, 1, 1, 1 );
-    var barMesh = new THREE.Mesh( barGeometry );
-    var barBSP = new ThreeBSP( barMesh );
+    // var barGeometry = new THREE.BoxGeometry( w, this.properties.depth, h, 1, 1, 1 );
+    // var barMesh = new THREE.Mesh( barGeometry );
+    // var barBSP = new ThreeBSP( barMesh );
 
     var cylinderGeometry = new THREE.CylinderGeometry( 32, 32, this.properties.depth, 64 );
     var cylinderMesh = new THREE.Mesh( cylinderGeometry );
     var cylinderBSP = new ThreeBSP( cylinderMesh );
-
 
     // ROUNDED
     // Rounded rectangle
@@ -38,12 +35,12 @@ var Room = window.Room = function(properties) {
     this.roundedRect(roundedRectShape, 0, 0, w, h, 2);
     var extrusionMesh = this.getExtrudedMesh( roundedRectShape, extrudeSettings, 0xffff, -w/2, -h/2, this.properties.depth/2, Math.PI/2, 0, 0, 1 );
     var extrusionBSP = new ThreeBSP( extrusionMesh );
-    w = 32;
-    h = 32;
-    var roundedRectShape = new THREE.Shape();
-    this.roundedRect(roundedRectShape, 0, 0, w, h, 2);
-    var extrusionMesh = this.getExtrudedMesh( roundedRectShape, extrudeSettings, 0xffff, -w/2, -h/2, this.properties.depth/2, Math.PI/2, 0, 0, 1 );
-    var roomExtrusionBSP = new ThreeBSP( extrusionMesh );
+    // w = 32;
+    // h = 32;
+    // var roundedRectShape = new THREE.Shape();
+    // this.roundedRect(roundedRectShape, 0, 0, w, h, 2);
+    // var extrusionMesh = this.getExtrudedMesh( roundedRectShape, extrudeSettings, 0xffff, -w/2, -h/2, this.properties.depth/2, Math.PI/2, 0, 0, 1 );
+    // var roomExtrusionBSP = new ThreeBSP( extrusionMesh );
     
     // subtract aperture shape from room
     var newBSP = cylinderBSP.subtract( extrusionBSP );
@@ -52,7 +49,6 @@ var Room = window.Room = function(properties) {
     this.roomMesh.position.set( 0, 0, this.properties.z );
     this.roomMesh.rotation.x = Math.PI/2;
     this.roomMesh.material.side = THREE.BackSide;
-
 
     // LIGHT
     this.light = new THREE.PointLight(0xffffff, 2, 200);
